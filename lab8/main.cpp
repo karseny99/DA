834242f6c756b3beb7769dbf7eb97b34d00c4cbe
karseny99 <arseny.karimov@gmail.com>
2024-11-10 22:03:32 +0300

    more unworking shit

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -2,71 +2,66 @@
 #include <iostream>
 #include <algorithm>
 #include <unordered_map>
-#include <list>
 
 int main() {
     std::ios::sync_with_stdio(false);
-    size_t size;
+    long long size;
     std::cin >> size;
 
-    std::vector<long long> nums(size);
-    std::vector<long long> tmpActual(size);
+    std::vector<long long> wrongNums(size);
+    std::vector<long long> actual(size);
+    // std::vector<long long> nums(size);
+    // std::vector<long long> tmpActual(size);
 
-    for(size_t i{0}; i < size; ++i) {
-        std::cin >> nums[i]; 
-        tmpActual[i] = nums[i];
-    }
+    // for(long long i{0}; i < size; ++i) {
+    //     std::cin >> nums[i]; 
+    //     tmpActual[i] = nums[i];
+    // }
 
-    std::sort(tmpActual.begin(), tmpActual.end());
 
-    std::vector<long long> wrongNums;
-    std::vector<long long> actual;
+    // std::sort(tmpActual.begin(), tmpActual.end());
 
-    size_t k{0};
-    for(size_t i{0}; i < size; ++i) {
-        if(nums[i] != tmpActual[i]) {
-            wrongNums.emplace_back(nums[i]);
-            actual.emplace_back(nums[i]);
-        }
+
+    // for(long long i{0}; i < size; ++i) {
+    //     if(nums[i] != tmpActual[i]) {
+    //         wrongNums.emplace_back(nums[i]);
+    //         actual.emplace_back(nums[i]);
+    //     }
+    // }
+
+    for(long long i{0}; i < size; ++i) {
+        std::cin >> wrongNums[i]; 
+        actual[i] = wrongNums[i];
     }
 
     std::sort(actual.begin(), actual.end());
 
-    size_t paired = 0, nonPaired = 0;
+    long long paired = 0, nonPaired = 0;
 
-    // 1 3 3 4 2 5 6 
-    // 1 2 3 3 4 5 6
+    for(long long i{0}; i < size; ++i) {
+        if(wrongNums[i] == actual[i]) continue;
 
-    std::unordered_map<long long, size_t> shifts;
-    std::unordered_map<size_t, bool> visited;
+        for(long long j{i + 1}; j < size; ++j) {
 
-    for(size_t i{0}; i < wrongNums.size(); ++i) {
-        if(wrongNums[i] == actual[i]) {
-            continue;
+            if(wrongNums[j] == actual[j]) continue;
+
+            if(wrongNums[i] == actual[j] and actual[i] == wrongNums[j]) {
+                std::swap(wrongNums[i], wrongNums[j]);
+                ++paired;
+                break;
+            }
+
         }
-
-        size_t firstPairPosition = (std::lower_bound(actual.begin(), actual.end(), wrongNums[i]) - actual.begin()) + shifts[wrongNums[i]]; 
-        size_t secondPairPosition = (std::lower_bound(actual.begin(), actual.end(), wrongNums[firstPairPosition]) - actual.begin()) + shifts[wrongNums[firstPairPosition]];
-
-        if(secondPairPosition == i) {
-            ++paired;
-            ++shifts[wrongNums[i]];
-            ++shifts[wrongNums[firstPairPosition]];
-            std::swap(wrongNums[i], wrongNums[firstPairPosition]);
-            continue;
-        }
-
-        ++nonPaired;
-        // ++shifts[wrongNums[i]];
     }
 
-    // 1 3 2
-    // 1 2 3
+    for(long long i{0}; i < size; ++i) {
+        if(wrongNums[i] != actual[i]) ++nonPaired;
+    }
 
     // std::cout << nonPaired << ' ' << paired << std::endl;   
 
-    size_t answer = nonPaired + paired;
-    if(nonPaired)
+    long long answer = nonPaired + paired;
+    if(nonPaired > 0)
         std::cout << answer - 1 << std::endl;
     else 
         std::cout << answer << std::endl;
