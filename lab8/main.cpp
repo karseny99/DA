0708fa8e48928c3ce11cbe8f9c6bcc7b26a4ccfa
karseny99 <arseny.karimov@gmail.com>
2024-11-11 21:30:32 +0300

    lab8 is done

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -2,67 +2,76 @@
 #include <iostream>
 #include <algorithm>
 #include <unordered_map>
+#include <array>
+
+namespace task {
+    const size_t sz = 4;
+
+    void solve() {
+        size_t size;
+        std::cin >> size;
+
+        std::vector<size_t> wrongNums(size);
+        std::array<size_t, sz> counting({0});
+
+
+        for(size_t i{0}; i < size; ++i) {
+            std::cin >> wrongNums[i]; 
+            ++counting[wrongNums[i]];
+        }
+
+        for(size_t i{sz - 1}; i >= 2; --i) 
+            counting[i] = counting[i - 1] + counting[i - 2];
+        counting[1] = 0;
+
+
+        size_t swaps = 0;
+
+        // processing only 2 in range of ones
+        for(size_t i{0}; i < counting[2]; ++i) {
+            if(wrongNums[i] != 2) continue;
+            for(size_t j{counting[2]}; j < size; ++j) {
+                if(wrongNums[j] == 1) {
+                    std::swap(wrongNums[i], wrongNums[j]);
+                    ++swaps;
+                    break;
+                }
+            }
+        }
+
+        // processing only 3 in range of ones
+        for(size_t i{0}; i < counting[2]; ++i) {
+            if(wrongNums[i] != 3) continue;
+            for(size_t j{size - 1}; j >= counting[2]; --j) {
+                if(wrongNums[j] == 1) {
+                    std::swap(wrongNums[i], wrongNums[j]);
+                    ++swaps;
+                    break;
+                }
+            }
+        }
+        // now range of ones is actually range of ones
+
+
+        // processing range of twos consisted only of 2 and 3, skipping 2 
+        // In fact, range of thress also consist only of 2 & 3
+        for(size_t i{counting[2]}; i < counting[3]; ++i) {
+            if(wrongNums[i] == 2) continue;
+
+            for(size_t j{size - 1}; j >= counting[3]; --j) {
+                if(wrongNums[j] == 2) {
+                    std::swap(wrongNums[i], wrongNums[j]);
+                    ++swaps;
+                    break;
+                }
+            }
+        }
+
+        std::cout << swaps << '\n';   
+    }
+};
 
 int main() {
     std::ios::sync_with_stdio(false);
-    long long size;
-    std::cin >> size;
-
-    std::vector<long long> wrongNums(size);
-    std::vector<long long> actual(size);
-    // std::vector<long long> nums(size);
-    // std::vector<long long> tmpActual(size);
-
-    // for(long long i{0}; i < size; ++i) {
-    //     std::cin >> nums[i]; 
-    //     tmpActual[i] = nums[i];
-    // }
-
-
-    // std::sort(tmpActual.begin(), tmpActual.end());
-
-
-    // for(long long i{0}; i < size; ++i) {
-    //     if(nums[i] != tmpActual[i]) {
-    //         wrongNums.emplace_back(nums[i]);
-    //         actual.emplace_back(nums[i]);
-    //     }
-    // }
-
-    for(long long i{0}; i < size; ++i) {
-        std::cin >> wrongNums[i]; 
-        actual[i] = wrongNums[i];
-    }
-
-    std::sort(actual.begin(), actual.end());
-
-    long long paired = 0, nonPaired = 0;
-
-    for(long long i{0}; i < size; ++i) {
-        if(wrongNums[i] == actual[i]) continue;
-
-        for(long long j{i + 1}; j < size; ++j) {
-
-            if(wrongNums[j] == actual[j]) continue;
-
-            if(wrongNums[i] == actual[j] and actual[i] == wrongNums[j]) {
-                std::swap(wrongNums[i], wrongNums[j]);
-                ++paired;
-                break;
-            }
-
-        }
-    }
-
-    for(long long i{0}; i < size; ++i) {
-        if(wrongNums[i] != actual[i]) ++nonPaired;
-    }
-
-    // std::cout << nonPaired << ' ' << paired << std::endl;   
-
-    long long answer = nonPaired + paired;
-    if(nonPaired > 0)
-        std::cout << answer - 1 << std::endl;
-    else 
-        std::cout << answer << std::endl;
+    task::solve();
 }
